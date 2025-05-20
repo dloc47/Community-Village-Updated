33cf913c960a77bd757db25a9bf9348913f0250f
Ankit Kumar <kumarankit0520@gmail.com>
2025-05-20 15:09:54 +0530

    implemented lazy loading for images gloabally with directive [lazy-loading]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './home/nav-bar/nav-bar.component';
-
+import { LazyLoadDirective } from './directives/lazy-load.directive';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet,NavBarComponent,LazyLoadDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/directives/lazy-load.directive.ts
+++ src/app/directives/lazy-load.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appLazyLoad]'
+  selector: 'img:not([loading]), iframe:not([loading])',
+  standalone: true
 })
-export class LazyLoadDirective {
 
-  constructor() { }
+export class LazyLoadDirective implements AfterViewInit {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  ngAfterViewInit(): void {
+    this.renderer.setAttribute(this.el.nativeElement, 'loading', 'lazy');
+  }
 }
