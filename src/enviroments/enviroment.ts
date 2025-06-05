8a7f820563358b82e21e44e434434ff5bff62b36
dloc47 <deepakchettri877@gmail.com>
2025-06-05 13:08:37 +0530

    Updated enviroment URLs

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://http://147.93.98.211/api' // Production API URL
+  // apiUrl: 'http://147.93.98.211/api' // Production API URL
     apiUrl: 'https://localhost:44344/api' // Localhost URL for development
 };
