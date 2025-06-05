205bab31c2c253bc9e28695f4153977ee25e1a49
dloc47 <deepakchettri877@gmail.com>
2025-06-05 13:06:53 +0530

    Updated enviroment.prod.ts

--- src/enviroments/enviroments.prod.ts
+++ src/enviroments/enviroments.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    // apiUrl: 'https://your-production-api.com/api'
-    apiUrl: 'http://147.93.98.211/api' // Prouduction API URL
+    apiUrl: 'http://147.93.98.211/api' // Production API URL
   };
