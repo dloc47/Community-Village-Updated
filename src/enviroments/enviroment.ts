1235f730cd140abbf3e03de2b4311898ecf71796
dloc47 <deepakchettri877@gmail.com>
2025-06-05 12:17:49 +0530

    Added production API URL in enviroment.ts and enviroment.prod.ts

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://localhost:44344/api' // Updated to new backend URL
     apiUrl: 'https://localhost:44344/api' // Localhost URL for development
+    // apiUrl: 'http://http://147.93.98.211/api' // Production API URL
 };
