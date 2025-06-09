c9cb3c5f9348e254c97aeca805342d22c7eaa464
Ankit Kumar <kumarankit0520@gmail.com>
2025-06-09 13:02:15 +0530

    updated search filter , Ui

--- src/app/search/search-main/search-main.component.ts
+++ src/app/search/search-main/search-main.component.ts
@@ -9,6 +9,9 @@ import { SearchActivityComponent } from "../search-activity/search-activity.comp
 import { SearchProductComponent } from "../search-product/search-product.component";
 import { SearchEventComponent } from "../search-event/search-event.component";
 import { ActivatedRoute } from '@angular/router';
+import { LucideAngularModule, ShoppingBag,
+   Users, X, Search, ChevronDown,Milestone ,Binoculars,HousePlus,
+   CalendarDays,TextSearch,ListFilter} from 'lucide-angular';
 
 @Component({
   selector: 'app-search-main',
@@ -19,13 +22,29 @@ import { ActivatedRoute } from '@angular/router';
     SearchVillageComponent, SearchHomestayComponent,
     SearchActivityComponent,
     SearchProductComponent,
-    SearchEventComponent
-]
+    SearchEventComponent,
+    LucideAngularModule
+  ]
 })
 export class SearchMainComponent implements OnInit {
   private fb=inject( FormBuilder)
   private searchService=inject( SearchService)
   private router=inject( ActivatedRoute)
+
+  // Define icons object
+  icons = {
+    VillageIcon: Users,
+    ActivityIcon: Binoculars,
+    ProductIcon: ShoppingBag,
+    EventIcon: CalendarDays,
+    LocationIcon: Milestone,
+    SearchIcon: Search,
+    TextSearchIcon:TextSearch,
+    ClearIcon: X,
+    DropdownIcon: ChevronDown,
+    HomestayIcon:HousePlus,
+    FilterIcon:ListFilter
+  }
 
   GlobalEnums = GlobalEnums;  //enums which store tabs category to avoid type error
   activeTab:GlobalEnums=GlobalEnums.village;  //current active tab or category to show data
@@ -203,6 +222,7 @@ setActiveTabParam(path: string) {
         searchTerm: '',
       }
     );
+    this.villages=[];
     this.getPaginatedData(this.paginateCategory,this.pageNo,this.itemPerPage);
    }
 
@@ -211,11 +231,14 @@ setActiveTabParam(path: string) {
     let villageStr = this.getValidInput(this.userInputs.get('village')?.value);
     let searchTerm = this.getValidInput(this.userInputs.get('searchTerm')?.value);
   
+
     // Convert to number if valid and not NaN
     let region = regionStr !== undefined && !isNaN(Number(regionStr)) ? Number(regionStr) : undefined;
     let village = villageStr !== undefined && !isNaN(Number(villageStr)) ? Number(villageStr) : undefined;
-  
-    this.getFilteredData(this.category, region, village, searchTerm);
+    
+    console.log(searchTerm)
+
+     this.getFilteredData(this.category, region, village, searchTerm);
   }
   
      // Function to check if the value is valid (not empty or just spaces)
