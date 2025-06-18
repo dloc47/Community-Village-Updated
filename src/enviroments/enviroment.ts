3bfa5080ae7c8e75f76404fa2f4c1bc6d771c98c
dloc47 <deepakchettri877@gmail.com>
2025-06-18 15:15:19 +0530

    Updated API URLs in enviorment.ts and enviroment.prod.ts

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  apiUrl: 'https://localhost:44344/api' // Localhost URL for development
+  // apiUrl: 'https://dpcvapi.sikkiminspires.in/api' // Production API URL
   // apiUrl: 'http://147.93.98.211/api' // Production API URL
-    apiUrl: 'https://localhost:44344/api' // Localhost URL for development
 };
