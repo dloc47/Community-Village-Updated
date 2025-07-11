79b48073aebee7a473e78210a6249c4e21035fe3
Ankit Kumar <kumarankit0520@gmail.com>
2025-07-11 22:38:47 +0530

    added footer , to the website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './common/nav-bar/nav-bar.component';
-
+import { FooterComponent } from './common/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent   {
+export class AppComponent {
   title = 'Community-Village';
-
 }
--- src/app/common/footer/footer.component.ts
+++ src/app/common/footer/footer.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { LucideAngularModule, MapPin, Phone, Mail } from 'lucide-angular';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  imports: [LucideAngularModule],
 })
 export class FooterComponent implements OnInit {
+  public icons = {
+    MapPin: MapPin,
+    Phone: Phone,
+    Mail: Mail,
+  };
 
-  constructor() { }
+  media = {
+    email: 'secy_tourismyahoo.com',
+    phone: '03592-232218, 03592-209090',
+    address: 'Paryatan Bhawan, Tadong, Gangtok, Sikkim 737102',
+  };
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
