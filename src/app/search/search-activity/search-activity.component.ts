f95fe1165f085ec99c4c9353ef9137f26771157d
Ankit Kumar <kumarankit0520@gmail.com>
2025-07-16 10:23:01 +0530

    updated search-activity , api calls, and search logic

--- src/app/search/search-activity/search-activity.component.ts
+++ src/app/search/search-activity/search-activity.component.ts
@@ -1,91 +1,108 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { paginatedEndpoints, search } from '../../utils/globalEnums.enum';
-import { getDistrictClass, getProfileImage ,handleImageError} from '../../utils/utils';
+import { Component, OnInit, inject } from '@angular/core';
+import {
+  getDistrictClass,
+  getProfileImage,
+  handleImageError,
+} from '../../utils/utils';
 import { SearchService } from '../../../services/search.service';
 import { RouterLink } from '@angular/router';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { CommonModule } from '@angular/common';
-import { LucideAngularModule, MapPin, Users,Tag, Binoculars, ChevronRight,HousePlus, Award } from 'lucide-angular';
-
+import {
+  LucideAngularModule,
+  MapPin,
+  Users,
+  Tag,
+  Binoculars,
+  ChevronRight,
+  HousePlus,
+  Award,
+} from 'lucide-angular';
+import { ApiService } from '../../../services/api.service';
+import { LoaderService } from '../../../services/loader.service';
+import { finalize, debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-search-activity',
   templateUrl: './search-activity.component.html',
   styleUrls: ['./search-activity.component.css'],
-  imports:[CommonModule,RouterLink,NgxPaginationModule,LucideAngularModule]
+  imports: [CommonModule, RouterLink, NgxPaginationModule, LucideAngularModule],
 })
 export class SearchActivityComponent implements OnInit {
+  private searchService = inject(SearchService);
+  private apiService = inject(ApiService);
+  private loader = inject(LoaderService);
+  public getProfileImage = getProfileImage;
+  public getDistrictClass = getDistrictClass;
+  public handleImageError = handleImageError;
 
- private searchService = inject(SearchService)
- public getProfileImage = getProfileImage;
- public getDistrictClass =getDistrictClass
- public handleImageError=handleImageError
+  activityData: any[] = [];
+  pagination = {
+    pageNo: 1,
+    pageSize: 0,
+    totalRecords: 0,
+  };
 
- itemPerPage=search.itemPerPage;
- pageNo:number=1
- activityData:any[]=[]
- totalRecords:number=0
- isDataFound=false;
- isLoading:boolean=false;
+  // Observables for template
+  isLoading$ = this.loader.isLoading$;
+  isDataFound$ = this.searchService.isDataFound$;
 
- // Define icons object
- icons = {
-   ArrowIcon: ChevronRight,
-   DistrictIcon: MapPin,
-   CommitteeIcon: Users,
-   TagIcon: Tag,
-   Users: Users,
-   HouseIcon: HousePlus,
-   BinocularsIcon:Binoculars,
-   Award: Award
- };
-
-
-
-  constructor() { }
+  // Define icons object
+  icons = {
+    ArrowIcon: ChevronRight,
+    DistrictIcon: MapPin,
+    CommitteeIcon: Users,
+    TagIcon: Tag,
+    Users: Users,
+    HouseIcon: HousePlus,
+    BinocularsIcon: Binoculars,
+    Award: Award,
+  };
 
   ngOnInit() {
-    this.searchService.queryState$.subscribe(({ type, query }) => {
-      this.loadData(type, query);
-    });
-
-    this.searchService.loading$.subscribe((isLoading) => {
-      this.isLoading = isLoading;
-    });
+    this.searchService.searchParams$
+      .pipe(
+        debounceTime(200),
+        distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b))
+      )
+      .subscribe((params) => {
+        this.pagination.pageSize = params.pageSize;
+        this.pagination.pageNo = params.pageNumber;
+        this.getActivities();
+      });
   }
 
-
-loadData(type: 'paginated' | 'filtered', query: any): void {
-  this.activityData=[];
-  this.totalRecords=0;
-
-  this.searchService.getData(type, query).subscribe((result: any) => {
-    if (result.isDataAvailable) {
-      this.activityData = result.items;
-      if(type=='paginated')
-      {
-        this.totalRecords = result.totalRecords;
-      }
-      else if(type=='filtered')
-      {
-        this.totalRecords = result.items.length;
-      }
-    } 
-  });
-}
-
-  
-  // ✅ Handle Page Change (ngx-pagination)
-onPageChange(pageNumber: number): void {
-  this.pageNo = pageNumber 
-  this.getPaginatedData(paginatedEndpoints.activities,this.pageNo,search.itemPerPage)
-  
-}
-
-   // ✅ Handle paginated data request
-   getPaginatedData(endpoint:paginatedEndpoints,pageNo: number, itemPerPage: number): void {
-    this.searchService.updateQueryState('paginated', { endpoint,pageNo, itemPerPage });
+  getActivities() {
+    this.loader.showLoader();
+    this.activityData = [];
+    const url = this.searchService.querySearchEndpoint();
+    this.apiService
+      .get(url)
+      .pipe(
+        finalize(() => {
+          this.loader.hideLoader();
+        })
+      )
+      .subscribe({
+        next: (res: any) => {
+          if (res) {
+            this.activityData = res.data;
+            this.pagination.totalRecords = res.totalRecords;
+            this.searchService.isDataFound.next(
+              !!(this.activityData && this.activityData.length)
+            );
+          }
+        },
+        error: (error: any) => {
+          console.error(error);
+          this.searchService.isDataFound.next(false);
+        },
+      });
   }
 
-
+  onPageChange(pageNumber: number): void {
+    this.searchService.updateParams({
+      pageNumber: pageNumber,
+    });
+  }
 }
