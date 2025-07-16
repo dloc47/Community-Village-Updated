637cee9ad1271fd02f778632a4432306e9c6c2c6
Ankit Kumar <kumarankit0520@gmail.com>
2025-07-16 10:38:25 +0530

    updated search event component  , api calls , search logic

--- src/app/search/search-event/search-event.component.ts
+++ src/app/search/search-event/search-event.component.ts
@@ -1,87 +1,106 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit,inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { getDynamicClass,getProfileImage, getDistrictClass } from '../../utils/utils';
+import {
+  getDynamicClass,
+  getProfileImage,
+  getDistrictClass,
+} from '../../utils/utils';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { SearchService } from '../../../services/search.service';
-import { paginatedEndpoints, search } from '../../utils/globalEnums.enum';
-import { IsNumberPipe } from "../../pipes/isNumber.pipe";
-import { LucideAngularModule, MapPin, Users, Tag, ChevronRight, Award } from 'lucide-angular';
+import {
+  LucideAngularModule,
+  MapPin,
+  Users,
+  Tag,
+  ChevronRight,
+  Award,
+} from 'lucide-angular';
+import { ApiService } from '../../../services/api.service';
+import { LoaderService } from '../../../services/loader.service';
+import { finalize, debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-search-event',
   templateUrl: './search-event.component.html',
   styleUrls: ['./search-event.component.css'],
-  imports: [CommonModule, RouterLink, NgxPaginationModule, LucideAngularModule]
+  imports: [CommonModule, RouterLink, NgxPaginationModule, LucideAngularModule],
 })
 export class SearchEventComponent implements OnInit {
+  private searchService = inject(SearchService);
+  private apiService = inject(ApiService);
+  private loader = inject(LoaderService);
+  public getProfileImage = getProfileImage;
+  public getDistrictClass = getDistrictClass;
+  public getClass = getDynamicClass;
 
- private searchService = inject(SearchService)
- public getProfileImage = getProfileImage;
- public getDistrictClass = getDistrictClass;
- public getClass = getDynamicClass;
+  eventData: any[] = [];
+  pagination = {
+    pageNo: 1,
+    pageSize: 0,
+    totalRecords: 0,
+  };
 
- itemPerPage=search.itemPerPage;
- pageNo:number=1
- eventData:any[]=[]
- totalRecords:number=0
- isDataFound=false;
- isLoading:boolean=false;
+  // Observables for template
+  isLoading$ = this.loader.isLoading$;
+  isDataFound$ = this.searchService.isDataFound$;
 
- // Define icons object
- icons = {
-   ArrowIcon: ChevronRight,
-   DistrictIcon: MapPin,
-   TagIcon: Tag,
-   Users: Users,
-   MapPin: MapPin,
-   Award: Award
- };
-
-  constructor() { }
+  // Define icons object
+  icons = {
+    ArrowIcon: ChevronRight,
+    DistrictIcon: MapPin,
+    TagIcon: Tag,
+    Users: Users,
+    MapPin: MapPin,
+    Award: Award,
+  };
 
   ngOnInit() {
-    this.searchService.queryState$.subscribe(({ type, query }) => {
-      this.loadData(type, query);
-    });
-
-    this.searchService.loading$.subscribe((isLoading) => {
-      this.isLoading = isLoading;
-    });
+    this.searchService.searchParams$
+      .pipe(
+        debounceTime(200),
+        distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b))
+      )
+      .subscribe((params) => {
+        this.pagination.pageSize = params.pageSize;
+        this.pagination.pageNo = params.pageNumber;
+        this.getEvents();
+      });
   }
 
-
-loadData(type: 'paginated' | 'filtered', query: any): void {
-  this.eventData=[];
-
-  this.searchService.getData(type, query).subscribe((result: any) => {
-    if (result.isDataAvailable) {
-      this.eventData = result.items;
-      if(type=='paginated')
-      {
-        this.totalRecords = result.totalRecords;
-      }
-      else if(type=='filtered')
-      {
-        this.totalRecords = result.items.length;
-      }
-    } 
-  });
-}
-
-
-  // ✅ Handle Page Change (ngx-pagination)
-onPageChange(pageNumber: number): void {
-  this.pageNo = pageNumber 
-  this.getPaginatedData(paginatedEndpoints.events,this.pageNo,search.itemPerPage)
-  
-}
-
-   // ✅ Handle paginated data request
-   getPaginatedData(endpoint:paginatedEndpoints,pageNo: number, itemPerPage: number): void {
-    this.searchService.updateQueryState('paginated', { endpoint,pageNo, itemPerPage });
+  getEvents() {
+    this.loader.showLoader();
+    this.eventData = [];
+    const url = this.searchService.querySearchEndpoint();
+    this.apiService
+      .get(url)
+      .pipe(
+        finalize(() => {
+          this.loader.hideLoader();
+        })
+      )
+      .subscribe({
+        next: (res: any) => {
+          if (res) {
+            this.eventData = res.data;
+            this.pagination.totalRecords = res.totalRecords;
+            this.searchService.isDataFound.next(
+              !!(this.eventData && this.eventData.length)
+            );
+          }
+        },
+        error: (error: any) => {
+          console.error(error);
+          this.searchService.isDataFound.next(false);
+        },
+      });
   }
 
+  onPageChange(pageNumber: number): void {
+    this.searchService.updateParams({
+      pageNumber: pageNumber,
+    });
+  }
 
   getDaysLeft(eventDate: string): string {
     const event = new Date(eventDate);
@@ -89,9 +108,5 @@ onPageChange(pageNumber: number): void {
     const diffTime = event.getTime() - today.getTime();
     const daysLeft = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
     return daysLeft <= 0 ? 'Ended' : `${daysLeft} days left`;
-  } 
-  
-  
-
-
+  }
 }
